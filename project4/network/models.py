fb1ecfed5057b57a0a66fdf9aef447bf533e89cb
Boondock19 <josematiasgonzalez20@gmail.com>
2020-12-07 12:36:39 -0400

    primer commit ya estan los models y las migrations

---
+++ project4/network/models.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Post(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="User_Post")
+    Contend=models.CharField(max_length=300)
+    Date=models.DateTimeField(auto_now_add=True)
+    like=models.ManyToManyField(User,blank=True,related_name="Post_likes")
+
+    def __str__(self):
+        return f"This is a Post created By {self.user.username}"
+
+class Profile(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="User_Profile")
+    Follower=models.ManyToManyField(User,blank=True,related_name="User_Follower")
+    Following=models.ManyToManyField(User,blank=True,related_name="User_Following")
+
+    def __str__(self):
+        return f"This is the profile of f{self.user.username}"
